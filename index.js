5b98c55043d736fc91114f5cf6acf73b72f7aa42
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-10-30 21:23:00 -0700

    set up server, GET base and users

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// implement your API here
+const express = require("express");
+const server = express();
+const database = require('./data/db.js');
+
+server.listen(3400, () => { 
+    console.log("server is running on port 3400");
+})
+
+//middleware
+server.use(express.json());
+
+server.get('/', (request, response) => {
+    response.send('Server is running!');
+})
+
+server.get('/api/users', (request, response) => {
+    database.find()
+        .then(users => {
+            response.status(200).json(users)
+        })
+        .catch(err => {
+            response.status(500).json({success: false, err, message: `request users failed`})
+        });
+});
